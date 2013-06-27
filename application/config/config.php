aa290b94e7fcdd6dd57e14a3b52cf7bcde03575e
Nicolas Crocfer <ncrocfer@gmail.com>
2013-06-27 02:11:25 +0200

    Changed base_url

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost/diffviewer';
+$config['base_url']	= 'http://diffviewer.shatter.fr';
 
 /*
 |--------------------------------------------------------------------------
